11da7dda285f51db0dfd057538ff2ffea0b80c48
Abhishek <cabhishek1304@gmail.com>
2024-07-01 13:35:08 +0530

    Longest Common Subsequence Length using Memoization Strings

---
+++ Arrays/Longest Common Subsequence.py
@@ -0,0 +1,31 @@
+#Using Memoization i.e saving the output of each function call possible in an array dp
+
+def lcs(x,y,m,n,dp):
+
+    if(m==0 or n==0):
+        dp[0][0]=0
+        return dp[0][0]
+    elif(dp[m][n]!=-1):
+       return dp[m][n]
+    elif(x[m-1]==y[n-1]):
+        dp[m][n]=1+lcs(x,y,m-1,n-1,dp)
+        return dp[m][n]
+    else:
+       t1=lcs(x,y,m-1,n,dp)
+       t2=lcs(x,y,m,n-1,dp)
+       if(t1>t2):
+        dp[m][n]=t1
+       else:
+        dp[m][n]=t2
+       return dp[m][n]
+# Driver code
+
+
+X = "AGGTAB"
+Y = "GXTXAYB"
+
+m = len(X)
+n = len(Y)
+dp = [[-1 for i in range(n + 1)]for j in range(m + 1)]
+
+print(f"Length of LCS is {lcs(X, Y, m, n, dp)}")
