fc11b93fedb621c7285d5923a1c20671981a3aa1
Abhishek Prashant Chandurkar <140307256+Ccode104@users.noreply.github.com>
2024-12-18 18:13:54 +0530

    Create  N by 2-Forest.cpp

---
+++ A2Z/Pattern based problems/N by 2-Forest.cpp
@@ -0,0 +1,16 @@
+void nForest(int n) {
+	// Write your code here.
+	
+	//Number of lines is 'n'
+	for(int i=0;i<n;i++)
+	{
+		//Number of stars in a row is 'i+1'
+		for(int j=0;j<=i;j++)
+		{
+			cout<<"* ";
+		}
+		cout<<endl;
+	}
+}
+
+//Reference : https://www.naukri.com/code360/problems/n-2-forest_6570178?utm_source=youtube&utm_medium=affiliate&utm_campaign=striver_patternproblems&leftPanelTabValue=SUBMISSION
